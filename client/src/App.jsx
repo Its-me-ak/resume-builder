e358e5a2e70cb6377d4d609893711754fd1da510
Mohd Aquib <akibkhan9984@gmail.com>
2025-10-31 12:40:40 +0530

    Implement routing and add core pages for resume builder application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import ResumeBuilder from './pages/ResumeBuilder'
+import Preview from './pages/Preview'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div className='text-3xl text-center text-red-500 mt-4 font-bold'>App</div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+        </Route>
+        <Route path="/view/:resumeId" element={<Preview />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App
