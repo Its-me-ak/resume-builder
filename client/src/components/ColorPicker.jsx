9f320faa8133ac1d862093bcb4bad05d0823404e
Mohd Aquib <akibkhan9984@gmail.com>
2025-11-27 15:32:03 +0530

    Refactor ColorPicker component for improved readability and close color picker on selection

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,47 +1,57 @@
-import { Check, Palette } from 'lucide-react'
-import React, { useState } from 'react'
+import { Check, Palette } from "lucide-react";
+import React, { useState } from "react";
 
-const ColorPicker = ({selectedColor, onChange}) => {
-const colors = [
-  { name: "Navy", value: "#1E3A8A" },
-  { name: "Royal", value: "#2563EB" },
-{ name: "Rose", value: "#E11D48" },
-  { name: "Slate", value: "#475569" },
-  { name: "Emerald", value: "#047857" },
-  { name: "Teal", value: "#0F766E" },
-  { name: "Burgundy", value: "#7F1D1D" },
-  { name: "Plum", value: "#6B21A8" },
-  { name: "Steel", value: "#0369A1" },
-  { name: "Coffee", value: "#78350F" },
-  { name: "Indigo", value: "#4338CA" },
-  { name: "Midnight", value: "#111827" },
-];
-    const [isOpen, setIsOpen] = useState(false)
+const ColorPicker = ({ selectedColor, onChange }) => {
+  const colors = [
+    { name: "Navy", value: "#1E3A8A" },
+    { name: "Royal", value: "#2563EB" },
+    { name: "Rose", value: "#E11D48" },
+    { name: "Slate", value: "#475569" },
+    { name: "Emerald", value: "#047857" },
+    { name: "Teal", value: "#0F766E" },
+    { name: "Burgundy", value: "#7F1D1D" },
+    { name: "Plum", value: "#6B21A8" },
+    { name: "Steel", value: "#0369A1" },
+    { name: "Coffee", value: "#78350F" },
+    { name: "Indigo", value: "#4338CA" },
+    { name: "Midnight", value: "#111827" },
+  ];
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className='relative'>
-        <button onClick={() => setIsOpen(prev => !prev)} className='flex items-center gap-1 text-sm text-purple-600 bg-linear-to-br from-purple-50 to-purple-100 ring-purple-300 hover:ring transition-all px-3 py-2 rounded-lg'>
-            <Palette size={16} className='max-sm:hidden'/> <span>Accent Color</span>
-        </button>
-        {isOpen && (
-            <div className='grid grid-cols-4 w-60 gap-2 absolute top-full left-0 right-0 p-3 mt-2 z-10 bg-white rounded-md border border-gray-200 shadow-sm'>
-                {
-                    colors.map((color) => (
-                        <div key={color.value} className='relative cursor-pointer group flex flex-col' onClick={() => onChange(color.value)}>
-                            <div className={`w-12 h-12 rounded-full border-2 border-transparent group-hover:border-black/25 transition-colors`} style={{backgroundColor: color.value}}>
-                            </div>
-                            {selectedColor === color.value && (
-                                <div className='absolute top-0 left-0 right-0 bottom-4 flex items-center justify-center'>
-                                    <Check className='size-5 text-white'/>
-                                </div>
-                            )}
-                            <p className='text-xs text-center mt-1 text-gray-600'>{color.name}</p>
-                        </div>
-                    ))
-                }
+    <div className="relative">
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="flex items-center gap-1 text-sm text-purple-600 bg-linear-to-br from-purple-50 to-purple-100 ring-purple-300 hover:ring transition-all px-3 py-2 rounded-lg"
+      >
+        <Palette size={16} className="max-sm:hidden" />{" "}
+        <span>Accent Color</span>
+      </button>
+      {isOpen && (
+        <div className="grid grid-cols-4 w-60 gap-2 absolute top-full left-0 right-0 p-3 mt-2 z-10 bg-white rounded-md border border-gray-200 shadow-sm">
+          {colors.map((color) => (
+            <div
+              key={color.value}
+              className="relative cursor-pointer group flex flex-col"
+              onClick={() => {onChange(color.value); setIsOpen(false);}}
+            >
+              <div
+                className={`w-12 h-12 rounded-full border-2 border-transparent group-hover:border-black/25 transition-colors`}
+                style={{ backgroundColor: color.value }}
+              ></div>
+              {selectedColor === color.value && (
+                <div className="absolute top-0 left-0 right-0 bottom-4 flex items-center justify-center">
+                  <Check className="size-5 text-white" />
+                </div>
+              )}
+              <p className="text-xs text-center mt-1 text-gray-600">
+                {color.name}
+              </p>
             </div>
-        )}
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ColorPicker
+export default ColorPicker;
