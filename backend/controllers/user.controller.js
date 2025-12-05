80860247dab3b999698ff5a0f58d75c0dc1fda8a
Mohd Aquib <akibkhan9984@gmail.com>
2025-12-05 17:06:47 +0530

    feat: add user login and retrieval functionality with authentication middleware

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -43,3 +43,48 @@ export const registerUser = async (req, res) => {
       .json({ message: "Internal server error", error: error.message });
   }
 };
+
+// controller for user login
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Password is incorrect" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+    return res.status(200).json({ message: "Login successfully", user: user });
+  } catch (error) {
+    console.log("Error in loginUser:", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+// controller for getting user by ID
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({ message: "User found", user: user });
+  } catch (error) {
+    console.log("Error in getUserById:", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+}
---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.model.js";
+
+export const protectedRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.JWT;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized - No token provided" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JSON_WEB_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken.userId).select("-password");
+    if (!user) {
+        return res.status(401).json({ message: "Unauthorized - User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
